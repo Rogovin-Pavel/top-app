44444415a994db6a3b98e57c8cf31f7d564eece3
p.rogovin <p.rogovin@devinotele.com>
2021-11-01 21:21:03 +0300

    Added grid to layout && footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import cn from 'classnames';
+import styles from './Footer.module.css';
 import { FooterProps } from './Footer.props';
+import { format } from 'date-fns';
 
-export const Footer = (props: FooterProps): JSX.Element => {
-  return <footer {...props}>Footer</footer>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</div>
+      <a href="#" target="_blank">
+        Пользовательское соглашение
+      </a>
+      <a href="#" target="_blank">
+        Политика конфиденциальности
+      </a>
+    </footer>
+  );
 };
